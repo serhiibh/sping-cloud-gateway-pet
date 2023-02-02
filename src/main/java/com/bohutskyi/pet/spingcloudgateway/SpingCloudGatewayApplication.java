dcef057323d161a3c06321edf72671ce8d66c535
Serhii Bohutskyi <sergey.boguckiy@gmail.com>
2023-02-02 15:09:01 +0100

    Initial commit

---
+++ src/main/java/com/bohutskyi/pet/spingcloudgateway/SpingCloudGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.bohutskyi.pet.spingcloudgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpingCloudGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpingCloudGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bohutskyi/pet/spingcloudgateway/SpingCloudGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bohutskyi.pet.spingcloudgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpingCloudGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
